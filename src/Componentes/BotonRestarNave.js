63793c7262ef81f75b4156783bcddeb8759a8c1c
LautaroGuardo <lautaro.guardo.99@gmail.com>
2022-11-09 21:13:42 -0300

    agrego escena GameOver del nuevo juego y nuevo boton para voler al menú

---
+++ src/Componentes/BotonRestarNave.js
@@ -0,0 +1,24 @@
+export class BotonRestartNave {
+
+    constructor(scene) {
+        this.relatedScene = scene;
+    }
+    preload() {
+        this.relatedScene.load.spritesheet('buttonN', 'img/Menuboton.png',
+            { frameWidth: 528, frameHeight: 162.5 }
+        );
+    }
+    create() {
+        this.startButton = this.relatedScene.add.sprite(400, 500, 'buttonN').setInteractive().setScale(.4);
+        this.startButton.on('pointerover', () => {
+            this.startButton.setFrame(1);
+        });
+       this.startButton.on('pointerout', () => {
+           this.startButton.setFrame(0);
+       });
+        this.startButton.on('pointerdown', () => {
+            
+            this.relatedScene.scene.start('game');
+        });
+    }
+}
