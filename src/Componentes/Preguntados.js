5a7a2dff9bb1559a673fb4d80f24fbb78239509f
LeonardoEmanuelAlfaro <balethleo@gmail.com>
2022-11-12 03:54:47 -0300

    mejora del juego preguntados, se agrego puntaje y tiempo para que pase automaticamente a la siguiente pregunta

--- src/Componentes/Preguntados.js
+++ src/Componentes/Preguntados.js
@@ -9,20 +9,20 @@ export default function Preguntados() {
     const [puntuacion, setPuntuacion] = useState(0);
     const [capRespCorrecta, setCapRespCorrecta] = useState(preguntas[preguntaActual].correcta);
     const [capResp, setCapRest] = useState('');
+    const [juegoHabilitado, SetJuegoHabilitado] = useState(true);
+    const [resultado, setResultado] = useState("");
+    const [bandera, SetBandera] = useState(true);
     //const [preguntaUsada, setPreguntaUsada] = useState('');
-    function Jugar() {
+    function Reiniciar() {
         setPreguntaActual(0);
-        //  setNumPregUsada(1);
-
-        // console.log(numPregUsada);
+        setPuntuacion(0);
         console.log("la pregunta es:" + preguntas[preguntaActual].pregunta);
+        SetJuegoHabilitado(true);
         /*  if (!preguntaElegida.includes(preguntaUsada)) {
              let ram = Math.floor(Math.random() * (5 - 0) + 0);
              console.log(ram);
              setPreguntaElegida(preguntas[ram].pregunta);
          } */
-
-
         //palabraMostrada.includes("-")
 
     }
@@ -31,25 +31,43 @@ export default function Preguntados() {
     function Siguiente() {
         //setNumPregUsada(numPregUsada + 1);
         setPreguntaActual(preguntaActual + 1);
+        SetJuegoHabilitado(true);
         // setCapRespCorrecta(preguntas[preguntaActual].correcta);
         //  console.log("la pregunta es siguiente es :" + preguntas[preguntaActual].pregunta);
     }
     function Verificar() {
-
-
-
         console.log(preguntaActual);
         console.log(capRespCorrecta);
         console.log(capResp);
-        if (capRespCorrecta == capResp) {
-            console.log("Respuesta Correcta");
-        } else console.log("Respuesta Incorrecta");
+        if (bandera) {
+            SetBandera(false);
+        }else{
+            if (capRespCorrecta == capResp) {
+                console.log("Respuesta Correcta");
+                setPuntuacion(puntuacion + 10);
+                setResultado("Respuesta Correcta");
+                SetJuegoHabilitado(false);
+                setTimeout(()=> {setResultado("")},2000);
+                setTimeout(()=>{setPreguntaActual(preguntaActual+1)},2000);
 
+
+    
+            } else {
+                setResultado("Respuesta Incorrecta");
+                console.log("Respuesta Incorrecta");
+                SetJuegoHabilitado(false);
+                setTimeout(()=> {setResultado("")},2000);
+                setTimeout(()=>{setPreguntaActual(preguntaActual+1)},2000);
+
+            }
+        }
+       
 
 
     }
     useEffect(() => { setCapRespCorrecta(preguntas[preguntaActual].correcta) }), [preguntaActual];
     useEffect(() => { Verificar() }, [capResp]);
+    useEffect(()=>{SetJuegoHabilitado(true)},[resultado]);
     //   useEffect(() => { Verificar() }, [capRespCorrecta]);
     //useEffect(() => {Verificar(preguntaActual)}, [capRespCorrecta]);
     return (
@@ -57,13 +75,15 @@ export default function Preguntados() {
         <>
             <h1>El Juego de Preguntados</h1>
             <span>pregunta {preguntaActual + 1} de {preguntas.length}</span>
+            <h3>su puntaje es:{puntuacion}</h3>
+            <h3>{resultado}</h3>
             <h2>La siguiente pregunta es: {preguntas[preguntaActual].pregunta}</h2>
-            <Button onClick={Jugar} >Jugar</Button>
+            <Button onClick={Reiniciar} >Reiniciar</Button>
             <Button onClick={Siguiente} >Siguiente</Button>
             <br></br>
             {preguntas[preguntaActual].opciones.map((valor, i) => (
                 //  <Button key={valor.op} onClick={() => Verificar(preguntaActual, valor.op)}> {valor.op} </Button>
-                <Button key={i} onClick={() => setCapRest(valor.op)}> {valor.op} </Button>
+                <Button key={i} onClick={() => setCapRest(valor.op)} disabled={!juegoHabilitado}> {valor.op} </Button>
             ))}
 
         </>
