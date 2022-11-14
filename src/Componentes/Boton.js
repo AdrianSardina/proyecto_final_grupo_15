94304f24db0a63b053c9c157606b1e3cc1ddc53b
LeonardoEmanuelAlfaro <balethleo@gmail.com>
2022-11-14 01:28:03 -0300

    Correccion de errores, mejora de codigo, y comentado, Alfaro Leonardo Emanuel

--- src/Componentes/Boton.js
+++ src/Componentes/Boton.js
@@ -1,20 +1,21 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 
-class Boton extends React.Component {
-    render() {
-        return (
-            <div className= "botones">
-        <Button href="/" variant="danger">Inicio</Button>
-        <Button href="/piedrapapeltijeras" variant="danger"> PiedraPapelTijeras</Button>
-        <Button href="/ahorcadito" variant="danger"> Ahorcadito</Button>
-        <Button href="/arkanoid" variant="danger">Arkanoid</Button>
-        <Button href="/preguntados" variant="danger">preguntados</Button>
-        <Button href="/nosotros" variant="danger">Nosotros</Button>
-        <Button href="/masjuegos" variant="danger">Mas Juegos</Button>
-        <Button href="/nave" variant="danger">Nave</Button>
+function Boton() {
+    return( 
+
+        <div className="botones">
+            <Button href="/" variant="danger">Inicio</Button>
+            <Button href="/piedrapapeltijeras" variant="danger"> PiedraPapelTijeras</Button>
+            <Button href="/ahorcadito" variant="danger"> Ahorcadito</Button>
+            <Button href="/arkanoid" variant="danger">Arkanoid</Button>
+            <Button href="/preguntados" variant="danger">Preguntados</Button>
+            <Button href="/nosotros" variant="danger">Nosotros</Button>
+            <Button href="/masjuegos" variant="danger">Mas Juegos</Button>
+            <Button href="/nave" variant="danger">Nave</Button>
 
         </div>
-    );
+    )
+    
 }
-//}
+export default Boton;
